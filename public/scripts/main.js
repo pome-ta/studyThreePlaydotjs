50463a41e704785f4c737f876a4a17fdf374c0a4
pome-ta <pome-ta@users.noreply.github.com>
2021-05-28 20:59:32 +0900

    01

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,45 +1,43 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 
-const width = document.querySelector('body').clientWidth;
-//const height = document.querySelector('body').clientHeight;
 
-const height = width;
+window.addEventListener('load', init);
 
-// レンダラーを作成
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
-
-// シーンを作成
-const scene = new THREE.Scene();
-
-// カメラを作成
-const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-camera.position.set(0, 0, +1000);
-
-// 箱を作成
-const geometry = new THREE.BoxGeometry(500, 500, 500);
-const material = new THREE.MeshPhongMaterial({color: 0xFF0000});
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
-
-// 平行光源
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-directionalLight.position.set(1, 1, 1);
-// シーンに追加
-scene.add(directionalLight);
-
-// 初回実行
-tick();
-
-function tick() {
-  requestAnimationFrame(tick);
+function init() {
+  const width = document.querySelector('body').clientWidth;
+  const height = width;
   
-  // 箱を回転させる
-  box.rotation.x += 0.01;
-  box.rotation.y += 0.01;
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#myCanvas')
+  });
   
-  // レンダリング
-  renderer.render(scene, camera);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+  
+  
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height);
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(400, 400, 400);
+  const material = new THREE.MeshNormalMaterial();
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+  
+  tick();
+  
+  // 毎フレーム時に実行されるループイベント
+  function tick() {
+    box.rotation.y += 0.01;
+    box.rotation.z += 0.02;
+    // レンダリング
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  }
+  
 }
