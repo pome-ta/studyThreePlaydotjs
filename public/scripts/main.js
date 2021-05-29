b88e03ac21ce8eb20fdb00c283638f37039932f8
pome-ta <pome-ta@users.noreply.github.com>
2021-05-29 21:30:09 +0900

    geometrys

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -21,29 +21,58 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1e4);
-  camera.position.set(0, 0, +1024);
+  camera.position.set(0, 512, +1024);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
   
-  const geometry = new THREE.SphereGeometry( 5, 32, 32 );
-  const material = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
-  const sphere = new THREE.Mesh( geometry, material );
-  scene.add( sphere );
+  // コンテナーを作成
+  const container = new THREE.Object3D();
+  scene.add(container);
   
-  // 平行光源
-  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+  // マテリアルを作成
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide,
+  });
+  
+  // 平行光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
   directionalLight.position.set(1, 1, 1);
-  // シーンに追加
   scene.add(directionalLight);
+  // 環境光を作成
+  const ambientLight = new THREE.AmbientLight(0x999999);
+  scene.add(ambientLight);
   
+  // ジオメトリを作成
+  const geometryList = [
+    new THREE.SphereGeometry(50), // 球体
+    new THREE.BoxGeometry(100, 100, 100), // 直方体
+    new THREE.PlaneGeometry(100, 100), // 平面
+    new THREE.TetrahedronGeometry(100, 0), // カプセル形状
+    new THREE.ConeGeometry(100, 100, 32), // 三角錐
+    new THREE.CylinderGeometry(50, 50, 100, 32), // 円柱
+    new THREE.TorusGeometry(50, 30, 16, 100), // ドーナツ形状
+  ];
+  
+  geometryList.map((geometry, index) => {
+    // 形状とマテリアルからメッシュを作成します
+    const mesh = new THREE.Mesh(geometry, material);
+  
+    // 3D表示インスタンスのsceneプロパティーが3D表示空間となります
+    container.add(mesh);
+  
+    // 円周上に配置
+    mesh.position.x = 400 * Math.sin((index / geometryList.length) * Math.PI * 2);
+    mesh.position.z = 400 * Math.cos((index / geometryList.length) * Math.PI * 2);
+  });
+
   tick();
-  
   // 毎フレーム時に実行されるループイベント
   function tick() {
-    requestAnimationFrame(tick);
-    sphere.rotation.y += 0.01;
+    container.rotation.y += 0.01;
     // レンダリング
     renderer.render(scene, camera);
+    
+    requestAnimationFrame(tick);
   }
   
 }
-
-
