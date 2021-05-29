2a1ca84665c5d28ce4381f01804f9af2a2ca40d7
pome-ta <pome-ta@users.noreply.github.com>
2021-05-29 11:42:04 +0900

    up

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -23,21 +23,10 @@ function init() {
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1e4);
   camera.position.set(0, 0, +1024);
   
-  // 球体を作成
-  const geometry = new THREE.SphereGeometry(300, 30, 30);
-  // 画像読み込み
-  const loader = new THREE.TextureLoader();
-  const texture = loader.load('../imgs/earthmap1k.jpg');
-  // マテリアルにテクスチャーを設定
-  const material = new THREE.MeshStandardMaterial({
-    map: texture
-  });
-  //const material = new THREE.MeshStandardMaterial({color: 0xFF0000});
-  //const material = new THREE.MeshNormalMaterial();
-  // メッシュを作成
-  const mesh = new THREE.Mesh(geometry, material);
-  // 3D空間にメッシュを追加
-  scene.add(mesh);
+  const geometry = new THREE.SphereGeometry( 5, 32, 32 );
+  const material = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
+  const sphere = new THREE.Mesh( geometry, material );
+  scene.add( sphere );
   
   // 平行光源
   const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
@@ -50,7 +39,7 @@ function init() {
   // 毎フレーム時に実行されるループイベント
   function tick() {
     requestAnimationFrame(tick);
-    mesh.rotation.y += 0.01;
+    sphere.rotation.y += 0.01;
     // レンダリング
     renderer.render(scene, camera);
   }
