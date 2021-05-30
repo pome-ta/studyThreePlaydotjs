7f89133af6b39875ce41795d6be8761d2b6bc27f
pome-ta <pome-ta@users.noreply.github.com>
2021-05-31 00:38:56 +0900

    color

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,15 +22,69 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1e4);
-  camera.position.set(0, 512, +1024);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
   
+  // 平行光源を作成
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
   
+  // マテリアルを作成
+  const material = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('../imgs/earthmap1k.jpg'),
+    side: THREE.DoubleSide,
+  });
+  
+  // 球体の形状を作成
+  const geometry = new THREE.SphereGeometry(300, 30, 30);
+  // 形状とマテリアルからメッシュを作成
+  const earthMesh = new THREE.Mesh(geometry, material);
+  // シーンにメッシュを追加
+  scene.add(earthMesh);
+  
+  // 星屑を作成 (カメラの動きをわかりやすくするため)
+  createStarField();
 
+  /** 星屑を作成 */
+  function createStarField() {
+    // 頂点情報を作成
+    const vertices = [];
+    for (let i = 0; i < 1000; i++) {
+      const x = 3000 * (Math.random() - 0.5);
+      const y = 3000 * (Math.random() - 0.5);
+      const z = 3000 * (Math.random() - 0.5);
+      vertices.push(x, y, z);
+    }
+    
+    // 形状データを作成
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    
+  
+    // マテリアルを作成
+    const material = new THREE.PointsMaterial({
+      size: 8,
+      color: 0xffffff,
+      blending: THREE.AdditiveBlending,
+    });
+  
+    // 物体を作成
+    const mesh = new THREE.Points(geometry, material);
+    scene.add(mesh);
+  }
+
+  
   tick();
   // 毎フレーム時に実行されるループイベント
-    renderer.render(scene, camera);
+  function tick() {
+    rot += 0.5; // 毎フレーム角度を0.5度ずつ加算
+    const radian = (rot * Math.PI) / 180;
+    // 角度に応じてカメラの位置を設定
+    camera.position.x = 1000 * Math.sin(radian);
+    camera.position.z = 1000 * Math.cos(radian);
     
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    // レンダリング
+    renderer.render(scene, camera);
     requestAnimationFrame(tick);
   }
   
