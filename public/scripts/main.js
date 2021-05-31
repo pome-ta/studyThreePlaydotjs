b7212d755f4a9b43801f5b9ced1bff24656f47bb
pome-ta <pome-ta@users.noreply.github.com>
2021-05-31 16:34:41 +0900

    add dust

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,7 @@
 // 最新version でやる気持ち
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
+//import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
 
 
 
@@ -36,6 +37,38 @@ function init() {
   // 形状とマテリアルからメッシュを作成します
   const mesh = new THREE.Mesh(new THREE.BoxGeometry(300, 300, 300), new THREE.MeshNormalMaterial());
   scene.add(mesh);
+  
+  
+  // 星屑を作成 (カメラの動きをわかりやすくするため)
+  createStarField();
+
+  /** 星屑を作成 */
+  function createStarField() {
+    // 頂点情報を作成
+    const vertices = [];
+    for (let i = 0; i < 1000; i++) {
+      const x = 3000 * (Math.random() - 0.5);
+      const y = 3000 * (Math.random() - 0.5);
+      const z = 3000 * (Math.random() - 0.5);
+      vertices.push(x, y, z);
+    }
+    
+    // 形状データを作成
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    
+  
+    // マテリアルを作成
+    const material = new THREE.PointsMaterial({
+      size: 4,
+      color: 0xffffff,
+      blending: THREE.AdditiveBlending,
+    });
+  
+    // 物体を作成
+    const mesh = new THREE.Points(geometry, material);
+    scene.add(mesh);
+  }
 
   
   
