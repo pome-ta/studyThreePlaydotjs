da7a8a51872acdbf265b8685650ab4538908ae6a
pome-ta <pome-ta@users.noreply.github.com>
2021-06-01 13:09:24 +0900

    lights

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -20,6 +20,8 @@ function init() {
     canvas: canvasElement,
     antialias: true,
   });
+  // レンダラー：シャドウを有効にする
+  renderer.shadowMap.enabled = true;
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
   
@@ -29,8 +31,8 @@ function init() {
 
   // カメラを作成
   const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1e4);
-  camera.position.set(0, 16, 16);
-  //camera.lookAt(new THREE.Vector3(0, 0, 0));
+  camera.position.set(32, 32, 32);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
   
   // カメラコントローラーを作成
   const controls = new OrbitControls(camera, canvasElement);
@@ -38,73 +40,47 @@ function init() {
   controls.enableDamping = true;
   controls.dampingFactor = 0.2;
   
-  
-  // 平行光源を作成
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
-  directionalLight.position.set(1, 1, 1);
-  scene.add(directionalLight);
-  // 環境光を追加
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
-  
-  // ポイント光源
-  const pointLight = new THREE.PointLight(0xffffff, 0.8, 1e4);
-  //pointLight.position.set(1, 1, 1);
-  scene.add(pointLight);
-  const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
-  scene.add(pointLightHelper);
-  
-  
-  
-  // .dae 呼び出し
-  // 3DS形式のモデルデータを読み込む
-  const loader = new ColladaLoader();
-  // 3dsファイルのパスを指定
-  loader.load('../models/collada/elf/elf.dae', (collada) => {
-    // 読み込み後に3D空間に追加
-    const model = collada.scene;
-    scene.add(model);
-  });
-  
-  
-  const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(64, 64), 
-    new THREE.MeshStandardMaterial(
-      {
-        color: 0x6699ff,
-        roughness:0.5,
-        //emissive: 0x072534,
-        side: THREE.FrontSide,
-        //flatShading: true,
-      })
+  // 床を作成
+  const meshFloor = new THREE.Mesh(
+    new THREE.BoxGeometry(2000, 0.1, 2000),
+    new THREE.MeshStandardMaterial({ color: 0x808080, roughness: 0.0 })
   );
-  ground.rotation.x = Math.PI / -2;
-  scene.add(ground);
+  // 影を受け付ける
+  meshFloor.receiveShadow = true;
+  scene.add(meshFloor);
+  
+  // オブジェクトを作成
+  const meshKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(3, 1, 100, 16),
+    new THREE.MeshStandardMaterial({ color: 0xaa0000, roughness: 0.0 })
+  );
+  meshKnot.position.set(0, 5, 0);
+  // 影を落とす
+  meshKnot.castShadow = true;
+  scene.add(meshKnot);
   
   
-  const lights = [];
-  lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-  lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-  lights[2] = new THREE.PointLight(0xffffff, 1, 0);
+  // 照明を作成
+  const light = new THREE.SpotLight(0xffffff, 2, 100, Math.PI / 4, 1);
+  // ライトに影を有効にする
+  light.castShadow = true;
+  light.shadow.mapSize.width = 2048;
+  light.shadow.mapSize.height = 2048;
+  scene.add(light);
  
-  lights[0].position.set(0, 200, 0);
-  lights[1].position.set(100, 200, 100);
-  lights[2].position.set(-100, -200, -100);
-  /*
-  scene.add(lights[0]);
-  scene.add(lights[1]);
-  scene.add(lights[2]);
-  */
+
   tick();
   // 毎フレーム時に実行されるループイベント
   function tick() {
     controls.update();
     
-    pointLight.position.set(
-      500 * Math.sin(Date.now() / 512),
-      500 * Math.sin(Date.now() / 1024),
-      500 * Math.cos(Date.now() / 512)
-    );
+  // 照明の位置を更新
+  const t = Date.now() / 500;
+  const r = 20.0;
+  const lx = r * Math.cos(t);
+  const lz = r * Math.sin(t);
+  const ly = 20.0 + 5.0 * Math.sin(t / 3.0);
+  light.position.set(lx, ly, lz);
     
     // レンダリング
     renderer.render(scene, camera);
